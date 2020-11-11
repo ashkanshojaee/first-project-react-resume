ecd461286877b2992be86c493a9fcdaf654684c4
unknown <ashkanshojaee48@gmail.com>
2020-11-12 02:33:35 +0330

    Updates

---
+++ precache-manifest.f6bd4beabbee3a9b5018d3822217475e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14513d4a9a2372ca4f95c530f589dc4c",
+    "url": "/first-project-react-resume/index.html"
+  },
+  {
+    "revision": "bcb7ed3942f1d7ecee17",
+    "url": "/first-project-react-resume/static/css/main.005fe9a2.chunk.css"
+  },
+  {
+    "revision": "80e8c175693db5afe221",
+    "url": "/first-project-react-resume/static/js/2.6b9213aa.chunk.js"
+  },
+  {
+    "revision": "ef19bb2eb003986230fd05ee786446b1",
+    "url": "/first-project-react-resume/static/js/2.6b9213aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb7ed3942f1d7ecee17",
+    "url": "/first-project-react-resume/static/js/main.4759aa2c.chunk.js"
+  },
+  {
+    "revision": "e286d371baec8bc7fa49",
+    "url": "/first-project-react-resume/static/js/runtime-main.ac133f78.js"
+  }
+]);
